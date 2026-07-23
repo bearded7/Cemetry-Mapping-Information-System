1f5786141947d7fa4f712d92ef42e4fe2ae87b58
Oriba Douglas <dragokods@gmail.com>
2026-07-23 08:27:58 +0200

    Update develop merge
    Fully Node JS

--- captcha/captcha.php
+++
@@ -1,5 +0,0 @@
-<?php
-include('captcha-gen.php');
-session_start();
-$captcha = new captcha();
-$_SESSION['captcha_keystring'] = $captcha->getKeyString();
--- contact_config.php
+++
@@ -1,4 +0,0 @@
-<?php
-// To
-define("WEBMASTER_EMAIL", 'email@companyname.com');
-?>
